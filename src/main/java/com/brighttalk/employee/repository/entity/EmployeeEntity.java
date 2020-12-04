ee949aeb8b432233d0e2be0131e4910edb3184cf
pjuliana2020 <75490658+pjuliana2020@users.noreply.github.com>
2020-12-04 14:19:52 -0500

    Create EmployeeEntity.java

---
+++ src/main/java/com/brighttalk/employee/repository/entity/EmployeeEntity.java
@@ -0,0 +1,86 @@
+package com.brighttalk.employee.repository.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class EmployeeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "first_name", nullable = false, length = 45)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false, length = 45)
+	private String lastName;
+
+	@Column(name = "email", nullable = false, length = 45)
+	private String email;
+
+	@Column(name = "phone", nullable = false, length = 45)
+	private String phone;
+	
+	
+	public EmployeeEntity(Long id, String firstName, String lastName, String email, String phone) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeEntity [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", phone=" + phone + "]";
+	}
+
+}
